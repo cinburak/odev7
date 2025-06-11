534f21c016d2b0afa1c0618254b82c44abb29087
cinburak <burakcin749@gmail.com>
2025-06-11 21:03:42 +0300

    Create about.js

---
+++ views/about.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hakkımda - Clean Blog</title>
+  <link rel="stylesheet" href="/css/style.css" />
+</head>
+<body>
+  <%- include('partials/_navigation') %>
+
+  <h1>Hakkımda sayfasına hoşgeldiniz</h1>
+  <p>Blog sahibi hakkında bilgiler burada olacak.</p>
+</body>
+</html>
