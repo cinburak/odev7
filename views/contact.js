1859f589d32c20ccd472b1107923e100c58ff1cb
cinburak <burakcin749@gmail.com>
2025-06-11 21:04:01 +0300

    Create contact.js

---
+++ views/contact.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8" />
+  <title>İletişim - Clean Blog</title>
+  <link rel="stylesheet" href="/css/style.css" />
+</head>
+<body>
+  <%- include('partials/_navigation') %>
+
+  <h1>İletişim sayfasına hoşgeldiniz</h1>
+  <p>İletişim bilgileri burada olacak.</p>
+</body>
+</html>
