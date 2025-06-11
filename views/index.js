1ce25e857a1768ce4dace5e5c8c6daeadb53aef6
cinburak <burakcin749@gmail.com>
2025-06-11 21:03:06 +0300

    Create index.js

---
+++ views/index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8" />
+  <title>Anasayfa - Clean Blog</title>
+  <link rel="stylesheet" href="/css/style.css" />
+</head>
+<body>
+  <%- include('partials/_navigation') %>
+
+  <h1>Anasayfa sayfasına hoşgeldiniz</h1>
+  <p>Burada blog yazıları listelenecek.</p>
+</body>
+</html>
